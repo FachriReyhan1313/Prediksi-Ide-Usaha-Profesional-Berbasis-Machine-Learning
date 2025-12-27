f0aa66bec116c45e0d687e7a45f771b1c35a9d2a
Fachri Reyhan <fahryreyhanantoni@gmail.com>
2025-12-28 01:10:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from controllers.prediction_controller import prediction_bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(prediction_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
